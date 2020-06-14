ad88f06d93181c4ea1f6d562aa8a6d0401d6a48d
Felix Garcia Lainez <fgarcialainez@gmail.com>
2020-06-14 12:42:55 +0200

    Completed challenge in Section 6: Kotlin Android Extensions

--- HabitTrainer/app/src/main/java/com/fgarcialainez/habittrainer/MainActivity.kt
+++ HabitTrainer/app/src/main/java/com/fgarcialainez/habittrainer/MainActivity.kt
@@ -2,10 +2,16 @@ package com.fgarcialainez.habittrainer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set data
+        iv_icon.setImageResource(R.drawable.water)
+        tv_title.setText(getString(R.string.drink_water_title))
+        tv_description.setText(getString(R.string.drink_water_description))
     }
 }
